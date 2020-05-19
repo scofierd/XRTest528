66732994041893d63eee2b109fc50cc1bf0427fd
以往XR <2205873638@qq.com>
2020-05-19 21:11:06 +0800

    1.第一次提交 包括podspec

---
+++ XRTest528/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XRTest528
+//
+//  Created by 以往XR on 2020/5/19.
+//  Copyright © 2020 以往XR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XRTest528/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  XRTest528
+//
+//  Created by 以往XR on 2020/5/19.
+//  Copyright © 2020 以往XR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
